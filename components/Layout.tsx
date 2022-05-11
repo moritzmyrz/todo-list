88157963edada018949bcc44b131a43b285f8359
Moritz André Myrseth <moritzmyrz@gmail.com>
2022-05-11 13:43:49 +0200

    fix

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,16 +1,14 @@
-import React from "react";
-import Header from "./sections/Header";
-import { Container } from "@chakra-ui/layout";
+import { Container } from '@chakra-ui/react';
+import React from 'react';
+import Header from './sections/Header';
 
 export default function Layout({ children }) {
-  return (
-    <>
-      <Header />
-      <Container>
-        <main>
-          {children}
-        </main>
-      </Container>
-    </>
-  )
+	return (
+		<>
+			<Header />
+			<Container>
+				<main>{children}</main>
+			</Container>
+		</>
+	);
 }
